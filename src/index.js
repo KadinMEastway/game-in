fa66d126918bd506b42602dfa4a8327c76bc7d51
Kadin Eastway <kadineastway@aarons-mbp.lan>
2021-08-31 11:49:48 -0400

    Add js arrays to represent games played and not played

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // document.getElementById("played").style.display = "none";
-document.getElementById("linkpar").innerHTML = "Hello HTML";
+// document.getElementById("linkpar").innerHTML = "Hello HTML";
+
+const listPlayed = ["Skyrim", "Zelda: Breath of the Wild", "Final Fantasy XV", "Fallout 4", "Ratchet and Clank"];
+const listUnplayed = ["Horizon Zero Dawn", "Borderlands 3", "Ratchet and Clank: Rift Apart", "Final Fantasy VII", "Octopath Traveler"];
