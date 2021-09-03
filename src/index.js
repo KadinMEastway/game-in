53a2a9d130658da2ab433e07b8d842051709b8a7
Kadin Eastway <keastway@kadins-air.lan>
2021-09-03 10:55:25 -0400

    add dependencies to package.json, append items to lists using javascript instead of HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// document.getElementById("played").style.display = "none";
-// document.getElementById("linkpar").innerHTML = "Hello HTML";
+const gamesPlayed = ["Skyrim", "Zelda: Breath of the Wild", "Final Fantasy XV", "Fallout 4", "Ratchet and Clank"];
+const gamesUnplayed = ["Horizon Zero Dawn", "Borderlands 3", "Ratchet and Clank: Rift Apart", "Final Fantasy VII", "Octopath Traveler"];
+const listPlayed = document.getElementById("played");
+const listUnplayed = document.getElementById("unplayed");
 
-const listPlayed = ["Skyrim", "Zelda: Breath of the Wild", "Final Fantasy XV", "Fallout 4", "Ratchet and Clank"];
-const listUnplayed = ["Horizon Zero Dawn", "Borderlands 3", "Ratchet and Clank: Rift Apart", "Final Fantasy VII", "Octopath Traveler"];
+function addToList(list, gameOrGames) {
+	if (typeof gameOrGames === "string") {
+		const listItem = document.createElement("li");
+		listItem.innerText = gameOrGames;
+		list.appendChild(listItem);
+	} else {
+		for (let i = 0; i < gameOrGames.length; i++) {
+			addToList(list, gameOrGames[i]);
+		}
+	}
+}
+
+addToList(listPlayed, gamesPlayed);
+addToList(listUnplayed, gamesUnplayed);
