f49257a92ec0cd4c5056cf8d2d4f3cff551b88e9
Kadin Eastway <keastway@kadins-air.lan>
2021-10-17 16:02:03 -0400

    refactored code, now single function for edit list instead of add or remove individually, and single function for all fetches used

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const listPlayed = document.getElementById("played-list");
 const listUnplayed = document.getElementById("unplayed-list");
-const d = new Date();
 const environment = {
 	urls: {
 		api: "http://localhost:3000"
@@ -11,12 +10,12 @@ function updateGameList(listName, gameOrGames, gameIndex = 0) {
 	const list = document.getElementById(`${listName}-list`);
 	if(typeof gameOrGames === "string") {
 		const listItem = document.createElement("li");
-		const removeButton = document.createElement("button");
-		removeButton.setAttribute("class", "removeButton");
-		removeButton.innerText = "-";
-		removeButton.addEventListener('click', () => { deleteGame(listName, gameIndex); });
+		const deleteButton = document.createElement("button");
+		deleteButton.setAttribute("class", "deleteButton");
+		deleteButton.innerText = "-";
+		deleteButton.addEventListener('click', () => { editList(listName, gameIndex, 'DELETE'); });
 		listItem.innerText = gameOrGames;
-		listItem.appendChild(removeButton);
+		listItem.appendChild(deleteButton);
 		list.appendChild(listItem);
 	} else {
 		for(let i = 0; i < gameOrGames.length; i++) {
@@ -25,57 +24,38 @@ function updateGameList(listName, gameOrGames, gameIndex = 0) {
 	}
 }
 
-async function addGame(listName, list) {
-	const newGame = prompt("Enter the name of the game you would like to add");
-	const newGameList = await postText(`add-${listName}-game`, newGame);
-	list.innerHTML = '';
-	updateGameList(listName, newGameList);
-}
-
 function changeDisplayTime() {
+	const d = new Date();
 	const hours = (d.getHours() % 12 === 0) ? 12 : d.getHours() % 12;
 	const minutes = (d.getMinutes() < 10) ? "0" + d.getMinutes() : d.getMinutes();
 	const seconds = (d.getSeconds() < 10) ? "0" + d.getSeconds() : d.getSeconds();
 	document.getElementById("currentTime").innerHTML = hours + ":" + minutes + ":" + seconds;
 }
 
-async function deleteGame(listName, index) {
+async function editList(listName, requestData = prompt("Enter the name of the game you would like to add"), editMethod = 'POST') {
 	const list = document.getElementById(`${listName}-list`);
-	const newGameList = await deleteText(`delete-${listName}-game`, index);
+	const editType = (editMethod === 'POST') ? 'add' : 'delete';
+	const newGameList = await improvedFetch(`${editType}-${listName}-game`, requestData, editMethod);
 	list.innerHTML = '';
 	updateGameList(listName, newGameList);
 }
 
-async function deleteText(api, index) {
-	const response = await fetch(`${environment.urls.api}/${api}`, {
-		body: index,
-		headers: { 'Content-Type': 'text/plain' },
-		method: 'DELETE'
-	});
-	return response.json();
-}
-
-async function getJSON(api) {
-	const response = await fetch(`${environment.urls.api}/${api}`);
-	return response.json();
-}
-
-async function postText(api, requestData) {
+async function improvedFetch(api, requestData = null, methodType = 'GET') {
 	const response = await fetch(`${environment.urls.api}/${api}`, {
 		body: requestData,
 		headers: { 'Content-Type': 'text/plain' },
-		method: 'POST'
+		method: methodType
 	});
 	return response.json();
 }
 
 async function main() {
-	const playedGamesPromise = getJSON("played-games");
-	const unplayedGamesPromise = getJSON("unplayed-games");
+	const playedGamesPromise = improvedFetch("played-games");
+	const unplayedGamesPromise = improvedFetch("unplayed-games");
 	const [ playedGames, unplayedGames ] = await Promise.all([ playedGamesPromise, unplayedGamesPromise ]);
 	updateGameList('played', playedGames);
 	updateGameList('unplayed', unplayedGames);
 
-	//setInterval(changeDisplayTime, 1000);
+	setInterval(changeDisplayTime, 1000);
 }
 main();
