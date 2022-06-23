5b59178c0f8feaaf25b808ac81665b2f346139c7
Kadin Eastway <eastwak3@star.lcc.edu>
2022-06-23 15:39:21 -0400

    added commentary, updated file paths

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const fs = require("fs");
-const app = express();
-const port = 3000;
-const fileDirectory = `${__dirname}/../data`;
+const express = require('express'); //requires the server to use express
+const fs = require("fs"); //requires the server to use node.js filesystem
+const app = express(); //express is a framework that lets you use HTTP methods
+const port = 3000; //port number is often used to help computers distinguish how to use the data that is transferred
+const fileDirectory = `${__dirname}/../data`; //shortened path
 
-function addGameToList(listName, game, res) {
-	fs.readFile(`${fileDirectory}/${listName}-games.json`, 'utf8', (err, games) => {
-		const list = JSON.parse(games);
-		if(game.trim() !== "") {
-			list.push(game.trim());
-			fs.writeFile(`${fileDirectory}/${listName}-games.json`, JSON.stringify(list), err => {
-				if(err) { console.error(err); }
-				console.log('Game list updated: \n' + list);
+function addGameToList(listName, game, response) { //
+	fs.readFile(`${fileDirectory}/${listName}-games.json`, 'utf8', (err, games) => { //grabs the JSON file
+		const list = JSON.parse(games); //parsing(converting) JSON file to JavaScript object
+		if(game.trim() !== "") { //making sure the name inputted isn't an empty string
+			list.push(game.trim()); //trims whitespace off name beginning and end, then pushes(adds) it to the end of the list object
+			fs.writeFile(`${fileDirectory}/${listName}-games.json`, JSON.stringify(list), err => { //converts list object back to JSON and updates list JSON file
+				if(err) { console.error(err); } //displays error message if needed
+				console.log('Game list updated: \n' + list); //displays updated game list if successfull
 			});
 		} else {
-			console.error('ERROR: invalid game name\n');
+			console.error('ERROR: invalid game name\n'); //displays error if game name is empty string
 		}
-		res.json(list);
+		response.json(list);
 	});
 }
 
-function deleteGameFromList(listName, index, res) {
+function deleteGameFromList(listName, index, response) {
 	fs.readFile(`${fileDirectory}/${listName}-games.json`, 'utf8', (err, games) => {
 		const list = JSON.parse(games);
 		list.splice(index, 1);
@@ -28,7 +28,7 @@ function deleteGameFromList(listName, index, res) {
 			if(err) { console.error(err); }
 			console.log('Game list updated: \n' + list);
 		});
-		res.json(list);
+		response.json(list);
 	});
 }
 
