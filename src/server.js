e8769ce76e525ad4567535b9c6b80c757bc29277
Kadin Eastway <kadineastway@kadins-mbp.lan>
2022-08-11 16:50:47 -0400

    merged add and remove functions on server side to be one function to reduce redundancy

--- src/server.js
+++ src/server.js
@@ -4,71 +4,63 @@ const app = express(); //express is a framework that lets you use HTTP methods
 const port = 3000; //port number is often used to help computers distinguish how to use the data that is transferred
 const fileDirectory = `${__dirname}/../data`; //shortened path
 
-function addGameToList(listName, game, response) { //
-	fs.readFile(`${fileDirectory}/${listName}-games.json`, 'utf8', (err, games) => { //grabs the JSON file
-		const list = JSON.parse(games); //parsing(converting) JSON file to JavaScript object
-		if(game.trim() !== "") { //making sure the name inputted isn't an empty string
-			list.push(game.trim()); //trims whitespace off name beginning and end, then pushes(adds) it to the end of the list object
-			fs.writeFile(`${fileDirectory}/${listName}-games.json`, JSON.stringify(list), err => { //converts list object back to JSON and updates list JSON file
-				if(err) { console.error(err); } //displays error message if needed
-				console.log('Game list updated: \n' + list); //displays updated game list if successfull
-			});
+function addOrRemoveGame(list, requestData, response) {
+	fs.readFile(`${fileDirectory}/${list}-games.json`, 'utf8', (err, games) => { //grabs the JSON file
+		const gameList = JSON.parse(games); //parsing(converting) JSON file to JavaScript object
+		if(requestData === "") {
+			console.error('ERROR: invalid game name\n'); //displays error if requestData is empty string
 		} else {
-			console.error('ERROR: invalid game name\n'); //displays error if game name is empty string
+			if(typeof requestData === "number") { //checks if it needs to add or remove a game based on if the requestData is a number or string
+				gameList.splice(requestData, 1); //splices (removes) the game at index 'requestData' from the list object
+			} else {
+				gameList.push(requestData.trim()); //trims whitespace off game beginning and end, then pushes(adds) it to the end of the list object
+			}
+			fs.writeFile(`${fileDirectory}/${list}-games.json`, JSON.stringify(gameList), err => { //overwrites list JSON file with updated list object
+				if(err) { console.error(err); } //displays error message if needed
+				console.log('Game list updated: \n' + gameList); //displays updated list object
+			});
 		}
-		response.json(list);
+		response.json(gameList); //returns list object
 	});
 }
 
-function deleteGameFromList(listName, index, response) {
-	fs.readFile(`${fileDirectory}/${listName}-games.json`, 'utf8', (err, games) => {
-		const list = JSON.parse(games);
-		list.splice(index, 1);
-		fs.writeFile(`${fileDirectory}/${listName}-games.json`, JSON.stringify(list), err => {
-			if(err) { console.error(err); }
-			console.log('Game list updated: \n' + list);
-		});
-		response.json(list);
-	});
-}
-
-app.use((request, response, next) => {
+app.use((request, response, next) => { //adding headers to allow front end to connect to back end
 	response.header('Access-Control-Allow-Origin', '*');
 	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	response.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
 	next();
 });
-app.use(express.json());
+app.use(express.json()); //allows backend to send or recieve either JSON or plain text
 app.use(express.text());
 
-app.get('/get-played-games', (request, response) => {
+app.get('/get-played-games', (request, response) => { //uses GET method to retrieve played games list and send it to front end
 	fs.readFile(`${fileDirectory}/played-games.json`, 'utf8', (err, playedGames) => {
 		response.send(playedGames);
 	});
 });
 
-app.get('/get-unplayed-games', (request, response) => {
+app.get('/get-unplayed-games', (request, response) => { //same as above, only with unplayed games list
 	fs.readFile(`${fileDirectory}/unplayed-games.json`, 'utf8', (err, unplayedGames) => {
 		response.send(unplayedGames);
 	});
 });
 
-app.post('/post-played-games', (request, response) => {
-	addGameToList('played', request.body, response);
+app.post('/post-played-games', (request, response) => { //uses POST method to retrieve played games list, add an item to it, and send it to front end
+	addOrRemoveGame('played', request.body, response);
 });
 
-app.post('/post-unplayed-games', (request, response) => {
-	addGameToList('unplayed', request.body, response);
+app.post('/post-unplayed-games', (request, response) => { //same as above, only with unplayed games list
+	addOrRemoveGame('unplayed', request.body, response);
 });
 
-app.delete('/delete-played-games', (request, response) => {
-	deleteGameFromList('played', parseInt(request.body), response);
+app.delete('/delete-played-games', (request, response) => { //uses DELETE method to retrieve played games list, remove an item from it, and send it to front end
+	addOrRemoveGame('played', parseInt(request.body), response);
 });
 
-app.delete('/delete-unplayed-games', (request, response) => {
-	deleteGameFromList('unplayed', parseInt(request.body), response);
+app.delete('/delete-unplayed-games', (request, response) => { //same as above, only with unplayed games list
+	addOrRemoveGame('unplayed', parseInt(request.body), response);
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //sets server up to listen at port 'port' (3000) and log its process
 	console.log(`Example app listening at http://localhost:${port}`);
 });
